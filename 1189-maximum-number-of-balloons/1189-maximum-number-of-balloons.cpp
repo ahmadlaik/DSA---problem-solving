85b9e7eff59b3ed31205d8501e4cba024eb6e77a
ahmadlaik <laikahm13@gmail.com>
2026-06-22 19:40:02 +0530

    Time: 0 ms (100%), Space: 8.9 MB (84.03%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+       int hash[26] ={0};
+        for(int i =0;i<text.size();i++){
+           int indx = text[i] -'a';
+           hash[indx]++;
+        }
+        int ans = min({  hash['b' - 'a'],
+                        hash['a' - 'a'],
+                         hash['l' - 'a']/2,
+                        hash['o'- 'a']/2,
+                        hash['n' -'a']
+                        
+
+        });
+        return ans;
+    }
+};
